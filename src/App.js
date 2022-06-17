ea09b24e8ff0c0ec0a6a41ac1635c347ad1ab4e7
bharodiyaisha <ishabharodiya298@gmail.com>
2022-06-17 11:47:07 +0530

    hooks example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Effect from "./Components/Hooks/Effect";
+import State from "./Components/Hooks/State";
+import Nav from "./Components/Hooks/Nav";
+import Param from "./Components/Hooks/Params";
+import Error from "./Components/Hooks/Error";
+import Ref from "./Components/Hooks/Ref";
+import ComA from "./Components/Hooks/Context/ComA";
+import Reducer from "./Components/Hooks/Reducer";
+import Memo from "./Components/Hooks/Memo";
+import Callback from "./Components/Hooks/Callback/Callback";
+
+import Test from "./Components/Hooks/ReactMemo/Test";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route index path="/" element={<State />} />
+          <Route path="/effect" element={<Effect />} />
+          <Route path="/params/:name" element={<Param />} />
+          <Route path="/ref" element={<Ref />} />
+          <Route path="/reducer" element={<Reducer />} />
+          <Route path="/memo" element={<Memo />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route path="/context" element={<ComA />} />
+          <Route path="/test" element={<Test />} />
+
+
+          <Route element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Test /> */}
     </div>
   );
 }
